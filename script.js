93afd175ce06edb463b8e153540753078439623d
apexskillscollege <ragkhan831@gmail.com>
2025-11-26 20:10:58 +0500

    Refactor verification link generation in script.js

--- script.js
+++ script.js
@@ -1,24 +1,22 @@
+const VERIFY_BASE = 'https://skillsboy474.github.io/Apex-Skills-College-/verify.html';
+
 function generateCertificate() {
 
     let name = document.getElementById("name").value;
     let course = document.getElementById("course").value;
     let date = document.getElementById("date").value;
 
-    // Auto Serial Number
     let serial = "ASC-" + Date.now();
 
-    // Make Verification Link
-    let link = "https://skillsboy474.github.io/apexskillscolleg/verify.html"
+    let link = VERIFY_BASE
         + "?n=" + encodeURIComponent(name)
         + "&c=" + encodeURIComponent(course)
         + "&d=" + encodeURIComponent(date)
         + "&s=" + serial;
 
-    // Show Link
     document.getElementById("result").innerHTML =
         "<b>Verification Link:</b><br>" + link;
 
-    // Generate QR
     let qr = new QRious({
         element: document.getElementById("qr"),
         size: 200,
